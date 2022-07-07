1a212e5bee6fe684108dba41b5f16fdf72fec02e
SRINATH-SETHUMADHAVAN <56186869+SRINATH-SETHUMADHAVAN@users.noreply.github.com>
2022-07-07 15:15:22 +0530

    added dependencies

---
+++ pmdRunner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PMDFILENAME="pmd-bin-6.33.0"
+if [ ! -d "$PMDFILENAME" ]; then
+  echo "You do not have PMD ready, downloading it."
+  curl -L -O https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.33.0/pmd-bin-6.33.0.zip
+  unzip $PMDFILENAME.zip
+  rm $PMDFILENAME.zip
+fi
+$PMDFILENAME/bin/run.sh pmd -d force-app -R ruleSet.xml
