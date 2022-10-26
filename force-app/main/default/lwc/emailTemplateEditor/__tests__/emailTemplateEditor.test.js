afa18d654144b80d0d75f712049ad79ea37e0f68
Vaibhav Baluni <104768659+vaibhav-baluni@users.noreply.github.com>
2022-10-26 15:53:34 +0530

    @W-11892982@ Send Core Email (#47)
    Send Email Core Feature - LWC for Email Template Editor, Invocable Action for sending the email.
    
    Design Doc

---
+++ force-app/main/default/lwc/emailTemplateEditor/__tests__/emailTemplateEditor.test.js
@@ -0,0 +1,190 @@
+import { createElement } from "lwc";
+import EmailTemplateEditor from "c/emailTemplateEditor";
+import getSenderAddresses from "@salesforce/apex/EmailTemplateEditorController.getSenderAddresses";
+import getEmailTemplates from "@salesforce/apex/EmailTemplateEditorController.getEmailTemplates";
+
+// Import mock data to send through the wire adapter.
+const mockGetEmailTemplates = require("./data/getEmailTemplates.json");
+const mockGetSenderAddresses = require("./data/getSenderAddresses.json");
+
+// Mock getSenderAddresses Apex wire adapter
+jest.mock(
+  "@salesforce/apex/EmailTemplateEditorController.getSenderAddresses",
+  () => {
+    const { createApexTestWireAdapter } = require("@salesforce/sfdx-lwc-jest");
+    return {
+      default: createApexTestWireAdapter(jest.fn())
+    };
+  },
+  { virtual: true }
+);
+
+// Mock getEmailTemplates Apex wire adapter
+jest.mock(
+  "@salesforce/apex/EmailTemplateEditorController.getEmailTemplates",
+  () => {
+    const { createApexTestWireAdapter } = require("@salesforce/sfdx-lwc-jest");
+    return {
+      default: createApexTestWireAdapter(jest.fn())
+    };
+  },
+  { virtual: true }
+);
+
+describe("c-email-template-editor", () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+
+    // Prevent data saved on mocks from leaking between tests
+    jest.clearAllMocks();
+  });
+
+  // Helper function to wait until the microtask queue is empty. This is needed for promise
+  // timing when calling imperative Apex.
+  async function flushPromises() {
+    return Promise.resolve();
+  }
+
+  it("render email template editor", async () => {
+    // Arrange
+    const element = createElement("c-email-template-editor", {
+      is: EmailTemplateEditor
+    });
+
+    // Act
+    document.body.appendChild(element);
+
+    getSenderAddresses.emit(mockGetSenderAddresses);
+    getEmailTemplates.emit(mockGetEmailTemplates);
+
+    // Wait for any asynchronous DOM updates
+    await flushPromises();
+  });
+
+  it("change folder", async () => {
+    const element = createElement("c-email-template-editor", {
+      is: EmailTemplateEditor
+    });
+
+    document.body.appendChild(element);
+
+    getSenderAddresses.emit(mockGetSenderAddresses);
+    getEmailTemplates.emit(mockGetEmailTemplates);
+
+    // Wait for any asynchronous DOM updates
+    await flushPromises();
+
+    const filters = element.shadowRoot.querySelectorAll("lightning-combobox");
+
+    filters[0].dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: "00lB00000010HR4IAM"
+        }
+      })
+    );
+  });
+
+  it("change template", async () => {
+    const element = createElement("c-email-template-editor", {
+      is: EmailTemplateEditor
+    });
+
+    document.body.appendChild(element);
+
+    getSenderAddresses.emit(mockGetSenderAddresses);
+    getEmailTemplates.emit(mockGetEmailTemplates);
+
+    // Wait for any asynchronous DOM updates
+    await flushPromises();
+
+    const filters = element.shadowRoot.querySelectorAll("lightning-combobox");
+
+    filters[0].dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: "00lB00000010HR4IAM"
+        }
+      })
+    );
+
+    filters[1].dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: "00XB0000000PSbTMAW"
+        }
+      })
+    );
+  });
+
+  it("change sender address", async () => {
+    const element = createElement("c-email-template-editor", {
+      is: EmailTemplateEditor
+    });
+
+    document.body.appendChild(element);
+
+    getSenderAddresses.emit(mockGetSenderAddresses);
+    getEmailTemplates.emit(mockGetEmailTemplates);
+
+    // Wait for any asynchronous DOM updates
+    await flushPromises();
+
+    const filters = element.shadowRoot.querySelectorAll("lightning-combobox");
+
+    filters[2].dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: "0D2B000000008yDKAQ"
+        }
+      })
+    );
+  });
+
+  it("test send email button click", async () => {
+    const element = createElement("c-email-template-editor", {
+      is: EmailTemplateEditor
+    });
+
+    document.body.appendChild(element);
+
+    getSenderAddresses.emit(mockGetSenderAddresses);
+    getEmailTemplates.emit(mockGetEmailTemplates);
+
+    // Wait for any asynchronous DOM updates
+    await flushPromises();
+
+    const emailSubject = element.shadowRoot.querySelectorAll("lightning-input");
+
+    emailSubject[0].dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: "Test Subject"
+        }
+      })
+    );
+
+    const emailBody = element.shadowRoot.querySelectorAll(
+      "lightning-input-rich-text"
+    );
+
+    emailBody[0].dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: "Test Body"
+        }
+      })
+    );
+
+    element.shadowRoot.querySelector(
+      'lightning-input[data-name="testEmail"]'
+    ).value = "test@aim.example.com";
+
+    const buttons = element.shadowRoot.querySelectorAll("lightning-button");
+
+    buttons[0].dispatchEvent(new CustomEvent("click"));
+  });
+});
---
+++ force-app/main/default/lwc/emailTemplateEditor/emailTemplateEditor.js
@@ -0,0 +1,150 @@
+import { api, LightningElement, track, wire } from "lwc";
+import getEmailTemplates from "@salesforce/apex/EmailTemplateEditorController.getEmailTemplates";
+import getSenderAddresses from "@salesforce/apex/EmailTemplateEditorController.getSenderAddresses";
+import sendTestEmail from "@salesforce/apex/EmailTemplateEditorController.sendTestEmail";
+import emailBody from "@salesforce/label/c.AIM_Email_Editor_Email_Body";
+import selectFolder from "@salesforce/label/c.AIM_Email_Editor_Select_Folder";
+import selectSender from "@salesforce/label/c.AIM_Email_Editor_Select_Sender";
+import selectTemplate from "@salesforce/label/c.AIM_Email_Editor_Select_Template";
+import sendEmailTest from "@salesforce/label/c.AIM_Email_Editor_Send_Test_Email";
+import subject from "@salesforce/label/c.AIM_Email_Editor_Subject";
+import testEmail from "@salesforce/label/c.AIM_Email_Editor_Test_Email";
+import genericError from "@salesforce/label/c.AIM_Generic_Error";
+
+export default class EmailTemplateEditor extends LightningElement {
+  @api whatId;
+  @api orgWideEmailAddressId;
+  @api subject;
+  @api emailBody;
+  @api templateId;
+  @track templatesByFolder;
+  @track folderOptions;
+  @track templateOptions;
+  @track senderEmailAddressesOptions;
+  folderValue;
+  errorMessage;
+  formats = [
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "list",
+    "indent",
+    "align",
+    "link",
+    "image",
+    "clean",
+    "table",
+    "header",
+    "color"
+  ];
+  label = {
+    emailBody,
+    selectFolder,
+    selectSender,
+    selectTemplate,
+    sendEmailTest,
+    subject,
+    testEmail
+  };
+
+  @wire(getEmailTemplates)
+  wiredEmailTemplates({ error, data }) {
+    if (data) {
+      this.templatesByFolder = data;
+      this.folderOptions = [];
+      for (const folderId of Object.keys(data)) {
+        this.folderOptions.push({
+          label: data[folderId].at(0).folderName,
+          value: folderId
+        });
+      }
+    } else if (error) {
+      this.errorMessage = genericError;
+      if (!Array.isArray(error) && !Array.isArray(error.body)) {
+        this.errorMessage = error.body.message;
+      }
+    }
+  }
+
+  @wire(getSenderAddresses)
+  wiredSenderAddresses({ error, data }) {
+    if (data) {
+      this.senderEmailAddressesOptions = [];
+      for (const addresses of data) {
+        this.senderEmailAddressesOptions.push({
+          label: `${addresses.DisplayName} (${addresses.Address})`,
+          value: addresses.Id
+        });
+      }
+    } else if (error) {
+      this.errorMessage = genericError;
+      if (!Array.isArray(error) && !Array.isArray(error.body)) {
+        this.errorMessage = error.body.message;
+      }
+    }
+  }
+
+  handleFolderChange(event) {
+    this.subject = "";
+    this.emailBody = "";
+    this.folderValue = event.detail.value;
+    const emailTemplates = this.templatesByFolder[this.folderValue];
+    this.templateOptions = [];
+    for (const emailTemplate of emailTemplates) {
+      this.templateOptions.push({
+        label: emailTemplate.emailTemplateName,
+        value: emailTemplate.emailTemplateId
+      });
+    }
+  }
+
+  handleEmailTemplateChange(event) {
+    this.subject = "";
+    this.emailBody = "";
+    this.templateId = event.detail.value;
+    for (const emailTemplate of this.templatesByFolder[this.folderValue]) {
+      if (emailTemplate.emailTemplateId === this.templateId) {
+        this.subject = emailTemplate.emailSubject;
+        this.emailBody = emailTemplate.emailBody;
+        break;
+      }
+    }
+  }
+
+  handleSenderChange(event) {
+    this.orgWideEmailAddressId = event.detail.value;
+  }
+
+  handleSubjectChange(event) {
+    this.subject = event.detail.value;
+  }
+
+  handleBodyChange(event) {
+    this.emailBody = event.detail.value;
+  }
+
+  handleSendTestEmail() {
+    const recipient = this.template.querySelector(
+      'lightning-input[data-name="testEmail"]'
+    ).value;
+    if (recipient) {
+      sendTestEmail({
+        emailAddress: recipient,
+        sender: this.orgWideEmailAddressId,
+        subject: this.subject,
+        body: this.emailBody,
+        whatId: this.whatId
+      })
+        .then((result) => {})
+        .catch((error) => {
+          this.errorMessage = genericError;
+          if (!Array.isArray(error) && !Array.isArray(error.body)) {
+            this.errorMessage = error.body.message;
+          }
+        });
+    }
+  }
+}
